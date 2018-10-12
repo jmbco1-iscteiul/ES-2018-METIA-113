ad949879eb4e6612216f2970c1159f8247cfe7ba
migigaspar <migigaspar@hotmail.com>
2018-10-12 13:49:35 +0100

    Interface Gráfica

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
 	public static void main(String[] args) {
+	interfacegrafica	interfacegrafica = new interfacegrafica();
+	interfacegrafica.open();
 		System.out.println("Hello World!!");
 	}
 }
