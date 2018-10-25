7484ee17a6c80164a50f43f68008f3193639116c
jpbao-iscteiul <jpbao@iscte-iul.pt>
2018-10-25 11:09:45 +0100

    Resolução de conflitos

--- src/MainWork/Main.java
+++ src/MainWork/Main.java
@@ -1,7 +1,7 @@
 package MainWork;
 
 public class Main {
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		
 		InterfaceGrafica i = new InterfaceGrafica();
 		i.open();
