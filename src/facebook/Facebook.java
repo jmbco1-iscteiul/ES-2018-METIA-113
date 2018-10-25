dcbc8e7d427db5252fc1a659b550a7f9159e0fb1
jmbco1-iscteiul <jmbco1@iscte-iul.pt>
2018-10-25 09:17:50 +0100

    facebook problem

--- src/facebook/Facebook.java
+++ src/facebook/Facebook.java
@@ -7,9 +7,9 @@ import com.restfb.types.User;
 
 public class Facebook {
 
-	private final static String accessToken="";//preencher pelo utilizador
-	private final static String ID="";
-	private final static String pass="";
+	private final static String accessToken="EAAD45TT4030BAAqmRlmlZBIE6yrm09US3SWzyysV3ESTZAAvBRFPfZCB4Yrwy49x3Jy0dLsy1KDgd2ZB9ZBRLiDfZCvSk4pqPbZBYLKZCZB4ZB2K7MIzRDqf8a1aK4q6ZB89ZCYFLmjaAUNnUHHaWrivATReq3NfodNgGpqZBvCyJe5JMIAZDZD";//preencher pelo utilizador
+	private final static String ID="273663319921533";
+	private final static String pass="dcc1e2de5a8a44804e89913f31dcd496";
 	
 	private FacebookClient fbClient;
 	
