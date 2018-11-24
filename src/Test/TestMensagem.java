e832777a0d7a8b1e2e5b0eb404673cf59b4e6215
jpbao-iscteiul <jpbao@iscte-iul.pt>
2018-11-24 22:18:38 +0000

    Alguns testes unitários validados

---
+++ src/Test/TestMensagem.java
@@ -0,0 +1,25 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import MainWork.SignIN;
+
+public class TestMensagem {
+	String[] m = null;
+	@Before
+	public void setUp()  {
+		String[] meses = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
+		m = meses;
+	}
+
+	@Test
+	public void testMes() {
+		int i = 2;
+		assertEquals("Mar", m[2]);
+	}
+
+	
+}
